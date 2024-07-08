22a97c9016fb46395ca2088e410e3f204106f668
devalmazbek <devalmazbek@gmail.com>
2024-07-08 15:01:37 +0600

    added component search order and query to get order by id

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function SearchOrder() {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(!query) return;
+        navigate(`/order/${query}`);
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="order" value={query} onChange={ e => setQuery(e.target.value) }/>
+        </form>
+    );
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,35 +1,47 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import CreateUser from './features/user/CreateUser';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, {loader as orderLoader} from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 export const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Home/>
-    },
-    {
-      path: '/menu',
-      element: <Menu/>
-    },
-    {
-      path: '/user',
-      element: <CreateUser/>
-    },
-    {
-      path: '/cart',
-      element: <Cart/>
-    },
-    {
-      path: '/order/new',
-      element: <CreateOrder />
-    },
-    {
-      path: '/order/:orderId',
-      element: <Order/>
-    },
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        },
+        {
+          path: '/menu',
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
+        {
+          path: '/user',
+          element: <CreateUser/>
+        },
+        {
+          path: '/cart',
+          element: <Cart/>
+        },
+        {
+          path: '/order/new',
+          element: <CreateOrder />
+        },
+        {
+          path: '/order/:orderId',
+          element: <Order/>,
+          loader: orderLoader,
+          errorElement: <Error />
+        },
+      ]
+    }
   ]);
